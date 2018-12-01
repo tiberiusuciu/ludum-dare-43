50efeba38ce0fa3f4985ff2f4f081e6d834ecadc
Guillaume Baril <barilgui@gmail.com>
2018-12-01 09:23:00 -0500

    Create game scene with nothing in it

--- src/index.js
+++ src/index.js
@@ -1,31 +1,24 @@
 import 'phaser';
+import GameScene from './scene/GameScene';
 
 var config = {
-    type: Phaser.AUTO,
-    parent: 'phaser-example',
+    type: Phaser.WEBGL,
+    parent: 'content',
+    pixelArt: true,
     width: 800,
     height: 600,
-    scene: {
-        preload: preload,
-        create: create
-    }
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: {
+                y: 500,
+                debug: true
+            }
+        }
+    },
+    scene: [
+        GameScene
+    ]
 };
 
-var game = new Phaser.Game(config);
-
-function preload () {
-    this.load.image('logo', 'assets/logo.png');
-}
-
-function create () {
-    var logo = this.add.image(400, 150, 'logo');
-
-    this.tweens.add({
-        targets: logo,
-        y: 450,
-        duration: 2000,
-        ease: 'Power2',
-        yoyo: true,
-        loop: -1
-    });
-}
+new Phaser.Game(config);
---
+++ src/scene/GameScene.js
@@ -0,0 +1,18 @@
+export default class GameScene extends Phaser.Scene {
+    preload() {
+        this.load.image('logo', 'assets/logo.png');
+    }
+    
+    create() {
+        var logo = this.add.image(400, 150, 'logo');
+    
+        this.tweens.add({
+            targets: logo,
+            y: 450,
+            duration: 2000,
+            ease: 'Power2',
+            yoyo: true,
+            loop: -1
+        });
+    }
+}
